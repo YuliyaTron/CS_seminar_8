d6a32027cf24eba3ea2016c7c47d99143e2b038b
yuliya tron <tron_yv@mail.ru>
2022-08-21 20:21:17 +0300

    Add Task_54/

---
+++ Task_54/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите минимальный элемент массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальный элемент массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + "   ");
+            else Console.Write(arr[i, j] + "|");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortDescendingStrings(int[,] arr)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < arr.GetLength(1) - 1; z++)
+        {
+            if (arr[i, z] < arr[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+            {
+                int temp = 0;
+                temp = arr[i, z];
+                arr[i, z] = arr[i, z + 1];
+                arr[i, z + 1] = temp;
+            }
+        }
+    }
+}
+}
+
+int[,] matrix = CreateMatrixRndInt(m, n, min, max);
+
+PrintMatrix(matrix);
+Console.WriteLine();
+
+SortDescendingStrings(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+
+
+
